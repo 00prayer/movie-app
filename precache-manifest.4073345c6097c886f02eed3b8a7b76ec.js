9f961560626037efc28584244fac451ce08a52e7
zeus <zeous83@nate.com>
2023-06-02 20:33:33 +0900

    Updates

---
+++ precache-manifest.4073345c6097c886f02eed3b8a7b76ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4951538a80aa01cd415aff95f1c0bd0",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "ad29e7d6c1588d015ed5",
+    "url": "/movie-app/static/css/main.abca9848.chunk.css"
+  },
+  {
+    "revision": "adf4faa9c0b6065f243b",
+    "url": "/movie-app/static/js/2.a175196b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie-app/static/js/2.a175196b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad29e7d6c1588d015ed5",
+    "url": "/movie-app/static/js/main.52027c2a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movie-app/static/js/main.52027c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab975dce6d97dccfcfa",
+    "url": "/movie-app/static/js/runtime-main.e337e9bd.js"
+  }
+]);
